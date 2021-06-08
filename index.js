51983a822faf67353338175d713a29ce8026a8fb
Jordan Bradley <jordanrhysbradley@gmail.com>
2021-06-08 19:33:36 +1000

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 
 let count = 0
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
 }
 
 function save() {
-console.log(count)
+let countSave = count + " - "
+saveEl.textContent += countSave
+countEl.textContent = 0
+count = 0
 }
